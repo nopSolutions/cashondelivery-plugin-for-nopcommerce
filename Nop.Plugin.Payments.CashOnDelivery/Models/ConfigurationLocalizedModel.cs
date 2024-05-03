4f5b9e53f9a8e94811324e548ef763356505f558
Alexey Anokhin <exile.developer@gmail.com>
2024-05-03 10:10:35 +0300

    Changes to work with nopCommerce 4.70

--- Nop.Plugin.Payments.CashOnDelivery/Models/ConfigurationLocalizedModel.cs
+++ Nop.Plugin.Payments.CashOnDelivery/Models/ConfigurationLocalizedModel.cs
@@ -1,13 +1,12 @@
 ﻿using Nop.Web.Framework.Models;
 using Nop.Web.Framework.Mvc.ModelBinding;
 
-namespace Nop.Plugin.Payments.CashOnDelivery.Models
-{
-    public record ConfigurationLocalizedModel : ILocalizedLocaleModel
-    {
-        [NopResourceDisplayName("Plugins.Payment.CashOnDelivery.DescriptionText")]
-        public string DescriptionText { get; set; }
+namespace Nop.Plugin.Payments.CashOnDelivery.Models;
 
-        public int LanguageId { get; set; }
-    }
+public record ConfigurationLocalizedModel : ILocalizedLocaleModel
+{
+    [NopResourceDisplayName("Plugins.Payment.CashOnDelivery.DescriptionText")]
+    public string DescriptionText { get; set; }
+
+    public int LanguageId { get; set; }
 }
--- Nop.Plugin.Payments.CashOnDelivery/Models/PaymentInfoModel.cs
+++ Nop.Plugin.Payments.CashOnDelivery/Models/PaymentInfoModel.cs
@@ -1,9 +1,8 @@
 ﻿using Nop.Web.Framework.Models;
 
-namespace Nop.Plugin.Payments.CashOnDelivery.Models
+namespace Nop.Plugin.Payments.CashOnDelivery.Models;
+
+public record PaymentInfoModel : BaseNopModel
 {
-    public record PaymentInfoModel : BaseNopModel
-    {
-        public string DescriptionText { get; set; }
-    }
+    public string DescriptionText { get; set; }
 }
